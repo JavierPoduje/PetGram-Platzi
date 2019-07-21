eb5196b99e8627e117658ff5331914b0ba3eef0a
JavierPoduje <javier.poduje@gmail.com>
2019-07-21 18:14:14 -0400

    Clase 34

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,14 +1,26 @@
 import React, { Fragment } from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
+import { RegisterMutation } from '../container/RegisterMutation'
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {({ activateAuth }) => {
       return (
         <Fragment>
-          <UserForm onSubmit={activateAuth} title='Registrarse' />
-          <UserForm onSubmit={activateAuth} title='Iniciar Sesión' />
+          <RegisterMutation>
+            {register => {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password }
+                const variables = { input }
+                register({ variables }).then(activateAuth)
+              }
+
+              return <UserForm title='Registrarse' onSubmit={onSubmit} />
+            }}
+          </RegisterMutation>
+
+          <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
         </Fragment>
       )
     }}
