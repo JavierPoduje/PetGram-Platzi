4f2203bf40cb7da6d741e5db28d495acbd0e8f9a
JavierPoduje <javier.poduje@gmail.com>
2019-07-21 22:26:22 -0400

    Clase 38 -> sessionStorage usado para recordar los likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react'
 import { Article, ImgWrapper, Img } from './styles'
 
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
@@ -10,9 +9,7 @@ import { Link } from '@reach/router'
 const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
 
   return (
@@ -28,13 +25,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
           <ToggleLikeMutation>
             {toggleLike => {
               const handleFavClick = () => {
-                !liked &&
-                  toggleLike({
-                    variables: {
-                      input: { id }
-                    }
-                  })
-                setLiked(!liked)
+                toggleLike({
+                  variables: {
+                    input: { id }
+                  }
+                })
               }
 
               return (
