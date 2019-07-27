b589e068c9a94cf140c304a5f4ef2861897eb78e
JavierPoduje <javier.poduje@gmail.com>
2019-07-27 19:27:40 -0400

    Fin del curso

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
 const path = require('path')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   output: {
@@ -19,6 +20,24 @@ module.exports = {
         {
           src: path.resolve('src/assets/icon.png'),
           sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            'https://(res.cloudinary.com|images.unsplash.com)'
+          ),
+          handler: 'CacheFirst',
+          options: { cacheName: 'images' }
+        },
+        {
+          urlPattern: new RegExp(
+            'https://petgram-server-puje-7y08ji9n2.now.sh/'
+          ),
+          handler: 'NetworkFirst',
+          options: { cacheName: 'api' }
         }
       ]
     })
